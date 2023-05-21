7f80637d6263b6a7bf811d6d2631b7e4176095a1
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-05-21 19:02:18 -0300

    fix: using mongo_url env var instead of .env file

--- lib/app/core/env/env.dart
+++ lib/app/core/env/env.dart
@@ -1,13 +1,7 @@
-import 'package:dotenv/dotenv.dart';
-
 abstract class Env {
   static late String mongoUrl;
 
   static Future<void> init([bool test = false]) async {
-    final env = DotEnv(includePlatformEnvironment: true)
-      ..load(
-        test ? ["test.env"] : [".env"],
-      );
-    mongoUrl = env["mongo_url"] ?? String.fromEnvironment("mongo_url");
+    mongoUrl = String.fromEnvironment("mongo_url");
   }
 }
